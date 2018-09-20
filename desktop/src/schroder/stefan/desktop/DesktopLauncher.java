bdb0f53b4340323e918d70727ae0f391fed8b9eb
Stefan-Schroder <stefans040297@outlook.com>
2018-09-20 22:57:21 +0200

    Project setup

---
+++ desktop/src/schroder/stefan/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package schroder.stefan.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import schroder.stefan.InfestationMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InfestationMain(), config);
+	}
+}
